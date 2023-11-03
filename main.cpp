1688608f24729ad043c1e342d4eb9d7e70fc69ea
Jmei340 <meij340@macomb.edu>
2023-11-02 22:45:12 -0400

    Added Inputs Section

--- main.cpp
+++ main.cpp
@@ -1,14 +1,35 @@
-// program name goes here
-// your name goes here
-// any example usage or helpful command line arguments go here
+// Shipping Cost
+// Programmers: Joey Mei, Ladawn Stuben
+// This program will determine shipping cost
 
 #include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
 // Add any other header files here
 
 ///////////////////////////////////////
 int main()
 {
-    // Work here
+    // Declaring Variable
+    char Item_Name;
+    char Fragile;
+    double Order_Total;
+    char Destination;
+
+    // Getting Inputs
+    cout << "Please enter the item name. (No Spaces)" << left << setw(10) << setfill('.') << right << ":", cin >> Item_Name;
+    cout << "Is the item fragile? (y=yes/n=no)" << left << setw(16) << setfill('.') << right << ":", cin >> Fragile;
+    cout << "Please enter your order total." << left << setw(19) << setfill('.') << right << ":", cin >> Order_Total;
+    cout << "Please enter destination. (usa/can/aus)" << left << setw(10) << setfill('.') << right << ":", cin >> Destination;
+
+    // If Statements
+
+
+    // Outputs
+
+
+    return 0;
 }
 
 // Place any unused code here so that the instructor can follow your problem solving thoughts.
