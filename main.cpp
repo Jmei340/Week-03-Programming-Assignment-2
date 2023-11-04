f8f81b1c9d0c1c47f441dbe0ccc743301740d3b2
LaDawn Stuben <105136560+ladawnstuben@users.noreply.github.com>
2023-11-04 12:42:06 -0400

    Add files via upload
    Added Output to file order.txt and added "Press any key" code to end program.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 // Shipping Cost
-// Programmers: Joey Mei, Ladawn Stuben
+// Week 3 Programming Assignment #2
+// Programmers: Joey Mei, LaDawn Stuben
 // This program will determine shipping cost
 
 #include <iostream>
@@ -7,13 +8,24 @@
 #include <string>
 #include <algorithm>
 #include <cmath>
+#include <fstream>
+
 using namespace std;
 // Add any other header files here
 
 ///////////////////////////////////////
 int main()
 {
-    // Declaring Variable
+    // Declaring Output File
+    ofstream outFile("order.txt"); 
+
+    // Check if file opened successfully
+    if (!outFile) {
+        cout << "Unable to open file for writing." << endl;
+        return 1;
+    }
+
+    // Declaring Variables
     string Item_Name;
     char Fragile;
     double Order_Total = 0.00;
@@ -29,11 +41,20 @@ int main()
     cout << endl;
     cout << endl;
 
+    // Print Header to File
+    outFile << "." << setw(48) << setfill('.') << right << "." << endl;
+    outFile << " ITCS 2530 - Programming Assignment for week #3" << endl;
+    outFile << "." << setw(48) << setfill('.') << right << "." << endl;
+    outFile << endl;
+    outFile << endl;
+
+
     // Getting Inputs
     cout << "Please enter the item name. (No Spaces)" << left << setw(10) << setfill('.') << right << ":";
     cin >> Item_Name;
     cout << "Is the item fragile? (y=yes/n=no)" << left << setw(16) << setfill('.') << right << ":";
     cin >> Fragile;
+
 
     // Validating Fragility
     if (Fragile != 'y' && Fragile != 'n') {
@@ -44,10 +65,13 @@ int main()
         return 1;
     }
 
+
+    // Getting Inputs
     cout << "Please enter your order total." << left << setw(19) << setfill('.') << right << ":";
     cin >> Order_Total;
     cout << "Please enter destination. (usa/can/aus)" << left << setw(10) << setfill('.') << right << ":";
     cin >> Destination;
+
 
     // Validating Destination
     if (Destination != "usa" && Destination != "can" && Destination != "aus") {
@@ -100,7 +124,7 @@ int main()
     transform(Item_Name.begin(), Item_Name.end(), Item_Name.begin(), ::toupper);
     transform(Destination.begin(), Destination.end(), Destination.begin(), ::toupper);
 
-    // Outputs
+    // Outputs to Screen
     cout << endl;
     cout << "Your item is" << left << setw(28) << setfill('.') << ".", cout << Item_Name << endl;
     cout << "Your shipping cost is" << left << setw(20) << setfill('.') << right << "$" << setprecision(2) << fixed << Shipping_Cost << endl;
@@ -108,6 +132,27 @@ int main()
     cout << "Your total shipping cost are" << left << setw(13) << setfill('.') << right << "$" << setprecision(2) << fixed << Total_Shipping << endl;
     cout << endl;
     cout << "-" << setw(49) << setfill('-') << right << "Thank You!" << endl;
+    cout << endl;
+    cout << endl;
+
+    //Prompt user to press key to exit
+    cout << "Press any key to continue . . .";
+    cin.ignore();
+    cin.get();
+
+    // Outputs to File
+    outFile << endl;
+    outFile << "Your item is" << left << setw(28) << setfill('.') << "." << Item_Name << endl;
+    outFile << "Your shipping cost is" << left << setw(20) << setfill('.') << right << "$" << setprecision(2) << fixed << Shipping_Cost << endl;
+    outFile << "You are shipping to" << left << setw(21) << setfill('.') << "." << Destination << endl;
+    outFile << "Your total shipping cost are" << left << setw(13) << setfill('.') << right << "$" << setprecision(2) << fixed << Total_Shipping << endl;
+    outFile << endl;
+    outFile << "-" << setw(49) << setfill('-') << right << "Thank You!" << endl;
+    outFile << endl;
+
+    // Closing File
+    outFile.close();
+
 
     return 0;
 }
